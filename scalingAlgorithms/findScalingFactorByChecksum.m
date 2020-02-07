ef20c4fa9508af3513037bf075c38682c0a6fa11
rj <rjpe@ieee.org>
2020-02-07 13:10:06 -0500

    fixes hardcoded number of sources in xsum

--- scalingAlgorithms/findScalingFactorByChecksum.m
+++ scalingAlgorithms/findScalingFactorByChecksum.m
@@ -1,7 +1,7 @@
 function [scalingFactor, scaledSourceEstimate] = ...
   findScalingFactorByChecksum(estimatedSource, base, degree, field, icaAlgo)
 
-% scale the estimate and pick the factor that produces a correct checksum
+% scale each packet estimate and pick the factor that produces a correct chksum
 
 alphabetSize = base^degree;
 numSources = size(estimatedSource, 1);
@@ -10,34 +10,42 @@ rowLength = size(estimatedSource, 2);
 results = zeros(alphabetSize, numSources);
 scalingFactor = zeros(numSources, 1);
 
-% for each ICA output run the scaling search algorithms
+% for each ICA output run the scaling search algorithm
 for iterEstSrc = 1 : numSources
-    % for each membber of the alphabet construct a test packet
-    for iterSclCoeff = 1 : alphabetSize - 1    
-        % for each information type run a scaling factor search
-        testSource = zeros(1, 40);
-        testSource(1,:) = ...
-            vecMultGf(iterSclCoeff, estimatedSource(iterEstSrc, 29:68), field, icaAlgo);
-        % check the IPv4 checksum of the scaled estimate 
-        results(iterSclCoeff, iterEstSrc) = verifyIPv4Checksum(testSource, 1);
-    end
+  % for each member of the alphabet construct a test packet
+  for iterSclCoeff = 1 : alphabetSize - 1    
+    % construct a test source (only the ipv4 checksum section of the packet)
+    testSource = zeros(1, 40);
+    testSource(1,:) = ...
+      vecMultGf(iterSclCoeff, estimatedSource(iterEstSrc, 29:68), field, icaAlgo);
+    % check & store the IPv4 checksum of the scaled estimate 
+    results(iterSclCoeff, iterEstSrc) = verifyIPv4Checksum(testSource, 1);
+  end
 end
 
-if size(find(results(:, 1), 1)) > 0
-    % pick the scaling coefficient which results in a correct checksum
-    scalingFactor(1,1) = find(results(:, 1), 1);
-else
-    scalingFactor(1,1) = 1;
+% for each packet pick the scaling factor which resulted in a correct chksum
+for iterSrc = 1 : numSources
+  if size(find(results(:, iterSrc), 1)) > 0
+    % success! store the correct scaling factor
+    scalingFactor(iterSrc,1) = find(results(:, iterSrc), 1);
+  else
+    % no such scaling coefficient found, store 1
+    scalingFactor(iterSrc,1) = 1;
+  end
 end
 
-if size(find(results(:, 2), 1)) > 0
-    scalingFactor(2,1) = find(results(:, 2), 1);
-else
-    scalingFactor(2,1) = 1;
-end
+%if size(find(results(:, 2), 1)) > 0
+%  scalingFactor(2,1) = find(results(:, 2), 1);
+%else
+%  scalingFactor(2,1) = 1;
+%end
 
 % construct the final scaled estimates
-scaledSourceEstimate(1,:) = ...
-  vecMultGf(scalingFactor(1), estimatedSource(1, :), field, icaAlgo);
-scaledSourceEstimate(2,:) = ...
-  vecMultGf(scalingFactor(2), estimatedSource(2, :), field, icaAlgo);
+for iterSrc = 1 : numSources
+  scaledSourceEstimate(iterSrc,:) = ...
+    vecMultGf(scalingFactor(iterSrc), estimatedSource(iterSrc, :), field, icaAlgo);
+end  
+
+%scaledSourceEstimate(2,:) = ...
+%  vecMultGf(scalingFactor(2), estimatedSource(2, :), field, icaAlgo);
+
